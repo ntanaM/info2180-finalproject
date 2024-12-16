83396b71e4c269410f3c5452d586f442ad674836
Dana Archer <danamarcher@gmail.com>
2024-12-15 22:48:33 -0500

    Added contact.css

--- contact.php
+++ contact.php
@@ -88,7 +88,7 @@ else{
 <title>Dolphin CRM</title>
 <meta name="viewport" content="width=device-width,initial-scale=1">
 
-<link rel="stylesheet" href="user_contact_design.css">
+<link rel="stylesheet" href="contact.css">
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
