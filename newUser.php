fc84b5866ae7e658adf14e8a6884fdc0c8d78783
Dana Archer <danamarcher@gmail.com>
2024-12-15 23:23:08 -0500

    Updated newUser css

--- newUser.php
+++ newUser.php
@@ -70,7 +70,7 @@ else{
 <title>Dolphin CRM</title>
 <meta name="viewport" content="width=device-width,initial-scale=1">
 
-<link rel="stylesheet" href="user_contact_design.css">
+<link rel="stylesheet" href="new_user.css">
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
